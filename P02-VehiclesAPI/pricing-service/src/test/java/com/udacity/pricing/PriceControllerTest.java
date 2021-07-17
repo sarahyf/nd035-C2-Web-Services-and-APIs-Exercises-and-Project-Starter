1378c49ceeda1f3f08b3add084dd2108df3680ad
sarahyf <alfadlysarah@gmail.com>
2021-07-17 12:24:38 +0300

    cleaning

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PriceControllerTest.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PriceControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,11 +24,8 @@ public class PriceControllerTest {
 
     @Test
     public void getPrice() throws Exception {
-    // MvcResult result = 
     mockMvc.perform(get(new URI("/services/price?vehicleId=" + 1)))
     .andExpect(status().isOk());
-    // .andReturn();
-    // verify(pricingService).getPrice(1);
-
     }
+    
 }
--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,49 +1,16 @@
 package com.udacity.pricing;
 
-
-// import org.junit.jupiter.api.Test;
-
-// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.udacity.pricing.api.PricingController;
-// import com.udacity.pricing.service.PricingService;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-// @RunWith(SpringRunner.class)
-// @WebMvcTest(PricingController.class)
-// @AutoConfigureMockMvc
-// @AutoConfigureJsonTesters
 public class PricingServiceApplicationTests {
-
-	// @Autowired
-	// private MockMvc mockMvc;
-	// @MockBean
-	// PricingService pricingService;
 
 	@Test
 	public void contextLoads() {
 	}
-
-	// @Test
-	// public void getPrice() throws Exception {
-	// 	MvcResult result = mockMvc.perform(get("/services/price?vehicleId=" + 1))
-	// 			.andExpect(status().isOk()).andReturn();
-	// 	// verify(pricingService).getPrice(1);
-
-	// }
-
-
+	
 }
